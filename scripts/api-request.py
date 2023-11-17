f474ecbee9c6675b9b062d7b1e3633cc1cd16e08
Rob Reid <reliablerascal@gmail.com>
2023-11-16 20:28:38 -0600

    update api-request.py to pre-parse JSON

--- scripts/api-request.py
+++ scripts/api-request.py
@@ -16,11 +16,30 @@ data = response.json()
 response
 print("requesting data from Divvy API")
 
+# get timestamp of dataset for filename
 central_tz = pytz.timezone('America/Chicago')
 status_timestamp = datetime.datetime.utcfromtimestamp(data['last_updated']).replace(tzinfo=pytz.utc)
 status_timestamp = status_timestamp.astimezone(central_tz).strftime('%Y_%m_%d_%I%M%p')
 
+#############
+# PREP DATA
+#############
+
+#read into dataframe and rename columns
 df_station_status= pd.DataFrame(data['data']['stations'])
+
+# pull out JSON fields
+df_station_status['n_classic'] = df_station_status['vehicle_types_available'].apply(
+    lambda x: next((item['count'] for item in x if item['vehicle_type_id'] == '1'), 0))
+df_station_status['n_electric'] = df_station_status['vehicle_types_available'].apply(
+    lambda x: next((item['count'] for item in x if item['vehicle_type_id'] == '2'), 0))
+df_station_status['n_scooters'] = df_station_status['vehicle_types_available'].apply(
+    lambda x: next((item['count'] for item in x if item['vehicle_type_id'] == '3'), 0))
+
+#remove unneeded fields
+df_station_status = df_station_status.drop(
+    columns=['vehicle_docks_available','last_reported','vehicle_types_available'],axis=1)
+
 filename = f"data/station_status_{status_timestamp}.csv"
 df_station_status.to_csv(filename, index=False)
 print(f"saving data as '{filename}'")
