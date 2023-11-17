79bf5ff4b7b677e0b17b50a090bda8e65ed493bd
Rob Reid <reliablerascal@gmail.com>
2023-11-16 20:46:29 -0600

    correct path to data folder
    and add one retrieved dataset

--- scripts/api-request.py
+++ scripts/api-request.py
@@ -46,6 +46,6 @@ df_station_status['n_scooters'] = df_station_status['vehicle_types_available'].a
 df_station_status = df_station_status.drop(
     columns=['vehicle_docks_available','last_reported','vehicle_types_available'],axis=1)
 
-filename = f"data/station_status_{status_timestamp}.csv"
+filename = f"../data/station_status_{status_timestamp}.csv"
 df_station_status.to_csv(filename, index=False)
 print(f"saving data as '{filename}'")
